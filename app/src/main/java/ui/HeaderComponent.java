c7d7eabe903395704f0a050615673d670e081502
Max <maxreichert@gmx.net>
2023-06-15 09:32:00 +0200

    HeaderComponent formats custom content correctly now

--- app/src/main/java/ui/HeaderComponent.java
+++ app/src/main/java/ui/HeaderComponent.java
@@ -7,26 +7,16 @@ import java.awt.*;
 public class HeaderComponent {
     public static JPanel createHeaderComponent(JComponent customContent, String title){
         JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new GridBagLayout());
+        mainPanel.setLayout(new BorderLayout());
 
         mainPanel.setBorder(new LineBorder(Color.green, 2));
-
-        GridBagConstraints gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.weightx = 1;
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
-        gridBagConstraints.anchor = GridBagConstraints.NORTH;
 
         JLabel header = new JLabel(title);
         header.setBackground(Color.red);
         header.setOpaque(true);
-        gridBagConstraints.gridy = 1;
-        gridBagConstraints.weighty = 0;
 
-        mainPanel.add(header, gridBagConstraints);
-        gridBagConstraints.gridy = 2;
-        gridBagConstraints.weighty = 24;
-        mainPanel.add(customContent, gridBagConstraints);
+        mainPanel.add(header, BorderLayout.NORTH);
+        mainPanel.add(customContent, BorderLayout.CENTER);
 
         return mainPanel;
     }
